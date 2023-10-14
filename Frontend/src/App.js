6ecb0e6b06846474cea3f6f87a1e3fd7913a2218
euodiatan <euodiatqy@gmail.com>
2023-10-14 13:47:46 +0800

    setup

---
+++ Frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <ChakraProvider>
+      <div className="App">
+      </div>
+    </ChakraProvider>
+  );
+}
+
+export default App;
